e15da0f4abfc22eafe6778a900e108d068219833
SrinivasPrasad <43960959+SrinuSandy@users.noreply.github.com>
2025-02-23 12:47:01 +0530

    Implement Room Joining and Creating Functionality

--- Assets/Scripts/Other/NetworkRunnerController.cs
+++ Assets/Scripts/Other/NetworkRunnerController.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NetworRunnerController : MonoBehaviour, INetworkRunnerCallbacks
+public class NetworkRunnerController : MonoBehaviour, INetworkRunnerCallbacks
 {
     [SerializeField] private NetworkRunner networkRunnerPreafab;
 
