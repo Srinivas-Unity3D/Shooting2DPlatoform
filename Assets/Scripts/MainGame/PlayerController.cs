069c123048763eb101375c12dfe02b8aee2f9fe2
SrinivasPrasad <43960959+SrinuSandy@users.noreply.github.com>
2025-02-23 17:15:32 +0530

    Enable Client Side Prediction and Implement Jump Funcitonality over network

--- Assets/Scripts/MainGame/PlayerController.cs
+++ Assets/Scripts/MainGame/PlayerController.cs
@@ -1,13 +1,22 @@
 using System;
+using System.Threading;
 using Fusion;
 using UnityEngine;
 
 public class PlayerController : NetworkBehaviour, IBeforeUpdate
 {
     [SerializeField] private float moveSpeed = 6;
+    [SerializeField] private float jumpForce = 1000;
 
+    [Networked] private NetworkButtons buttonPrev { get; set; }
     private float horizontal; 
     private Rigidbody2D rigid;
+
+    private enum PlayerInputButtons 
+    {
+        None,
+        Jump
+    }
 
     public override void Spawned()
     {
@@ -29,13 +38,26 @@ public class PlayerController : NetworkBehaviour, IBeforeUpdate
         if (Runner.TryGetInputForPlayer<PlayerData>(Object.InputAuthority, out PlayerData input))
         {
             rigid.velocity = new Vector2(input.HorizontalInput * moveSpeed, rigid.velocity.y);
+            CheckJumpInput(input);
         }
+    }
+
+    private void CheckJumpInput(PlayerData input) 
+    {
+        NetworkButtons pressed = input.networkButtons.GetPressed(buttonPrev);
+        if (pressed.WasPressed(buttonPrev, PlayerInputButtons.Jump)) 
+        {
+            rigid.AddForce(Vector2.up * jumpForce, ForceMode2D.Force);
+        }
+
+        buttonPrev = input.networkButtons;
     }
 
     public PlayerData GetPlayerNetworkInput()
     {
         PlayerData data = new PlayerData();
         data.HorizontalInput = horizontal;
+        data.networkButtons.Set(PlayerInputButtons.Jump, Input.GetKey(KeyCode.Space));
         return data;
     }
 }
